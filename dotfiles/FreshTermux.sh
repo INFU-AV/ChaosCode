554543b9f41f6206064518b0d2762f388f48566b
INFU <66220663+INFU-AV@users.noreply.github.com>
2022-09-23 10:01:24 +0100

    Proper recovery (yeah, with git)

--- dotfiles/FreshTermux.sh
+++ dotfiles/FreshTermux.sh
@@ -12,12 +12,11 @@ echo " --------------"
 # echo "-3)------AUDIO-"
 
 yes | pkg update ; yes | pkg install exa emacs man ttyd sox git neofetch ack ffmpeg python ncdu p7zip termux-api ; termux-setup-storage
-
-cd "~"
+cd ~
 git clone  https://github.com/INFU-AV/ChaosCode
 cd ChaosCode/dotfiles
-mv -f .termux "~"
-mv -f .bashrc "~"
-mv -f .emacs.d "~"
-cd "~"
-rm -fr ChaosCode
+mv -f .termux ~/.termux
+mv -f .bashrc ~/.bashrc
+mv -f .emacs.d ~/.emacs.d
+cd ~
+rm -fr ChaosCode/
